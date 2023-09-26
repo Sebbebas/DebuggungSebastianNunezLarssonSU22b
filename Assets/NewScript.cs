8dcbebdc33d1aca9ef3010887528c862aa06d6bc
Sebastian Nuñez Larsson <Sebbebas1005@gmail.com>
2023-09-26 14:38:36 +0200

    Unity Project

---
+++ Assets/NewScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    void Start  ()
+    {
+        Debug.Log("this project is a complete mess");
+    }
+}
